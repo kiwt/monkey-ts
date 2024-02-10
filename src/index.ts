18b79228f2cd03b21e6de4d63949e4553dd70095
kiwt <ksiwt.jp@gmail.com>
2024-02-10 14:40:41 +0900

    restrcture entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { start } from "./src/repl/repl";
+import { start } from "./repl/repl";
 
 console.log("Hello mrnugget! This is the Monkey programming language!");
 console.log("Feel free to type in commands");
